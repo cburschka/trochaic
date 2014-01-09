42cddaa11dcb27dc2afda9ac66c142e674bae433
Christoph Burschka <christoph@burschka.de>
2014-01-09 03:56:12 +0100

    Create a formatText() function.
    This function finds {var} and [var] placeholders and
    replaces them with the escaped or raw value of the
    given variable, respectively.

--- visual.js
+++ visual.js
@@ -1,4 +1,12 @@
 visual = {
+  formatText: function(text, variables) {
+    text = text.replace(/\{([a-z]+)\}|\[([a-z]+)\]/g, function(rep, plain, raw) {
+      var key = plain || raw;
+      return variables[key] ? (plain ? visual.textPlain(variables[key]) : variables[key]) : rep;
+    });
+    return text;
+  },
+
   textPlain: function(text) {
     var replacers = {'<': '&lt;', '>': '&gt;', '&':'&amp;'};
     return text.replace(/[<>&]/g, function(x) { return replacers[x]; });
