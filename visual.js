7ea192f9d8e438559dbfe046ca621f9db67e6afb
Christoph Burschka <christoph@burschka.de>
2014-01-19 00:28:44 +0100

    Rewrite formatText with formatting types.
    All variables passed to formatText now have a type, which is
    either their name or (for multiple variables of the same type)
    a dotted prefix. A variable named {type} or {type.x} will be
    passed through visual.format.type() before output.

--- visual.js
+++ visual.js
@@ -1,32 +1,38 @@
 visual = {
+  format: {
+    plain: function(text) {
+      var replacers = {'<': '&lt;', '>': '&gt;', '&':'&amp;'};
+      return text.replace(/[<>&]/g, function(x) { return replacers[x]; });
+    },
+
+    raw: function(text) {
+      return raw;
+    }
+  },
+
   /**
    * Poor man's sprintf, with some features from Drupal's t().
    * Splice variables into a template, optionally escaping them.
    *
-   * @param {string} text A format string with placeholders like {a} and [b].
+   * @param {string} text A format string with placeholders like {a}.
    * @param {Object} variables A hash keyed by variable name.
    *
    * Any placeholder with a corresponding variable will be replaced.
-   * If the placeholder is in curly brackets, the variable will be HTML-escaped.
+   * If the variable is named "<f>.x", then it will be passed through format.<f>().
+   *
    * @return {string} The rendered text.
    */
   formatText: function(text, variables) {
-    text = text.replace(/\{([a-z]+)\}|\[([a-z]+)\]/g, function(rep, plain, raw) {
-      var key = plain || raw;
-      return typeof variables[key] == 'string' ? (plain ? visual.textPlain(variables[key]) : variables[key]) : rep;
+    for (var key in variables) if (!variables[key]) delete variables[key];
+    for (var key in variables) {
+      var m = /([a-z]+)\.[a-z]+/.exec(key);
+      var type = m ? m[1] : key;
+      type = this.format[type] ? type : 'plain';
+      variables[key] = this.format[type](variables[key]);
+    }
+    text = text.replace(/\{([a-z\.]+)\}/g, function(rep, key) {
+      return typeof variables[key] == 'string' ? variables[key] : rep;
     });
     return text;
-  },
-
-  /**
-   * Escape < and > in a text.
-   *
-   * Wherever possible, this function should be avoided in favor of DOM
-   * and jQuery methods like $.text() and Text().
-   * Only use it when working on strings.
-   */
-  textPlain: function(text) {
-    var replacers = {'<': '&lt;', '>': '&gt;', '&':'&amp;'};
-    return text.replace(/[<>&]/g, function(x) { return replacers[x]; });
   }
 };
