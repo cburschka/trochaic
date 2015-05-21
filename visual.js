68494c8559c7718936d5a0947ee93b61c3bbbf47
Christoph Burschka <christoph@burschka.de>
2015-05-21 14:14:05 +0200

    Cleaner string formatting.
    This is a code-style patch that
    
    a) transforms all "{format.var}" placeholders into "{format:var}".
    b) Allows both format and variable names to be \w+ rather than just [a-z]+
    c) Removes the format specifier from the variable name as it is passed to formatText().
    d) Cleans up formatText() a bit.

--- visual.js
+++ visual.js
@@ -18,28 +18,23 @@ visual = {
   },
 
   /**
-   * Poor man's sprintf, with some features from Drupal's t().
-   * Splice variables into a template, optionally escaping them.
+   * Splice variables into a template with format identifiers.
    *
-   * @param {string} text A format string with placeholders like {a}.
+   * @param {string} text A format string with placeholders like {name1} and {format:name2}.
    * @param {Object} variables A hash keyed by variable name.
    *
    * Any placeholder with a corresponding variable will be replaced.
-   * If the variable is named "<f>.x", then it will be passed through format.<f>().
-   *
+   * The variable will be processed either by the specified format, or the one
+   * matching its name, or the "plaintext" formatter by default.
    * @return {string} The rendered text.
    */
   formatText: function(text, variables) {
-    for (var key in variables) if (!variables[key]) delete variables[key];
-    for (var key in variables) {
-      var m = /([a-z]+)\.[a-z]+/.exec(key);
-      var type = m ? m[1] : key;
-      type = this.format[type] ? type : 'plain';
-      variables[key] = this.format[type](variables[key]);
-    }
-    text = text.replace(/\{([a-z\.]+)\}/g, function(rep, key) {
-      return typeof variables[key] == 'string' ? variables[key] : rep;
+    return text.replace(/{(?:(\w+):)?(\w+)}/g, function(rep, format, key) {
+      if (key in variables) {
+        var out = (visual.format[format || key] || visual.format.plain)(variables[key]);
+        if (typeof out == 'string') return out;
+      }
+      return rep;
     });
-    return text;
   }
 };
