dec0433303a92d77cbe6b0d03a890f2418be3503
Christoph Burschka <christoph@burschka.de>
2015-12-21 03:57:26 +0100

    Rewrite text formatting with DOM.
    The entire text formatting code (which previously worked with
    HTML strings) has been replaced with a new system that works with
    DOM nodes directly.

--- visual.js
+++ visual.js
@@ -1,40 +1,28 @@
 visual = {
-  format: {
-    /**
-     * Escape < and > in a text.
-     *
-     * Wherever possible, this function should be avoided in favor of DOM
-     * and jQuery methods like $.text() and Text().
-     * Only use it when working on strings.
-     */
-    plain: function(text) {
-      var replacers = {'<': '&lt;', '>': '&gt;', '&': '&amp;', '"': '&quot;', "'": '&apos;'};
-      return text ? text.replace(/[<>&"']/g, function(x) { return replacers[x]; }) : '';
-    },
-
-    raw: function(text) {
-      return text;
-    }
-  },
+  format: {},
 
   /**
    * Splice variables into a template with format identifiers.
    *
    * @param {string} text A format string with placeholders like {name1} and {format:name2}.
    * @param {Object} variables A hash keyed by variable name.
+   * @param {bool} html Optional flag that causes text to be evaluated as HTML.
    *
    * Any placeholder with a corresponding variable will be replaced.
    * The variable will be processed either by the specified format, or the one
    * matching its name, or the "plaintext" formatter by default.
    * @return {string} The rendered text.
    */
-  formatText: function(text, variables) {
-    return text.replace(/{(?:(\w+):)?(\w+)}/g, function(rep, format, key) {
-      if (key in variables) {
-        var out = (visual.format[format || key] || visual.format.plain)(variables[key]);
-        if (typeof out == 'string') return out;
-      }
-      return rep;
-    });
+  formatText: function(text, variables, html) {
+    return $('<span>')[html ? 'html' : 'text'](text)
+      .replaceText(/({(?:(\w+):)?(\w+)})/g, function(rep, format, key) {
+        if (key in variables) {
+          if ((format || key) in visual.format) {
+            return visual.format[format || key](variables[key]);
+          }
+          return variables[key];
+        }
+        return rep;
+      });
   }
 };
