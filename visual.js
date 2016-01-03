e0e70cc1ba8e9b58e6051959d632db6a7704c539
Christoph Burschka <christoph@burschka.de>
2016-01-03 03:15:48 +0100

    Add comments to visual.js

--- visual.js
+++ visual.js
@@ -1,12 +1,30 @@
 visual = {
+  /**
+   * Poor man's sprintf, with some features from Drupal's t().
+   * Splice variables into a template, optionally escaping them.
+   *
+   * @param {string} text A format string with placeholders like {a} and [b].
+   * @param {Object} variables A hash keyed by variable name.
+   *
+   * Any placeholder with a corresponding variable will be replaced.
+   * If the placeholder is in curly brackets, the variable will be HTML-escaped.
+   * @return {string} The rendered text.
+   */
   formatText: function(text, variables) {
     text = text.replace(/\{([a-z]+)\}|\[([a-z]+)\]/g, function(rep, plain, raw) {
       var key = plain || raw;
-      return variables[key] ? (plain ? visual.textPlain(variables[key]) : variables[key]) : rep;
+      return typeof variables[key] == 'string' ? (plain ? visual.textPlain(variables[key]) : variables[key]) : rep;
     });
     return text;
   },
 
+  /**
+   * Escape < and > in a text.
+   *
+   * Wherever possible, this function should be avoided in favor of DOM
+   * and jQuery methods like $.text() and Text().
+   * Only use it when working on strings.
+   */
   textPlain: function(text) {
     var replacers = {'<': '&lt;', '>': '&gt;', '&':'&amp;'};
     return text.replace(/[<>&]/g, function(x) { return replacers[x]; });
